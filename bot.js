2fafc71723552b0656c1152c0512749691ab06cf
ZeroTheKing <44266197+ZeroTheKing@users.noreply.github.com>
2018-11-15 16:54:23 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var prefix = "S";
-var adminprefix = '1'
+var adminprefix = 'S'
 const developers = ["511915236014686230"]
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
@@ -9,22 +9,22 @@ client.on('message', message => {
       
   if (message.content.startsWith(adminprefix + 'setg')) {
     client.user.setGame(argresult);
-      message.channel.send(By Ayman   ${argresult}**`)
+      message.channel.send(`**?   ${argresult}**`)
   } else 
      if (message.content === (adminprefix + "leave")) {
     message.guild.leave();        
   } else  
   if (message.content.startsWith(adminprefix + 'setw')) {
   client.user.setActivity(argresult, {type:'WATCHING'});
-      message.channel.send(`By Ayman   ${argresult}**`)
+      message.channel.send(`**?   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'setl')) {
   client.user.setActivity(argresult , {type:'LISTENING'});
-      message.channel.send(`By Ayman   ${argresult}**`)
+      message.channel.send(`**?   ${argresult}**`)
   } else 
   if (message.content.startsWith(adminprefix + 'sets')) {
     client.user.setGame(argresult, "https://www.twitch.tv/One");
-      message.channel.send(`By Ayman`)
+      message.channel.send(`**?**`)
   }
   if (message.content.startsWith(adminprefix + 'setname')) {
   client.user.setUsername(argresult).then
